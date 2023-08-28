ec42c310a9d3988b91831a57a407a8dea470a2a3
Joseph Kirkish <kirkish.2@gmail.com>
2023-08-27 22:24:23 -0400

    Tidy up code
    Tidy up code

--- src/main/java/com/coderscampus/springsecuritypractice/domain/User.java
+++ src/main/java/com/coderscampus/springsecuritypractice/domain/User.java
@@ -5,10 +5,24 @@ import java.util.Collection;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users") // overrides the default table name from `user` to `users`
 public class User implements UserDetails {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
     private String username;
     private String password;
+    
+    public User () {}
     
     public User(String username, String password) {
         super();
@@ -24,6 +38,10 @@ public class User implements UserDetails {
     @Override
     public String getPassword() {
         return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     @Override
--- src/main/java/com/coderscampus/springsecuritypractice/service/UserService.java
+++ src/main/java/com/coderscampus/springsecuritypractice/service/UserService.java
@@ -3,19 +3,16 @@ package com.coderscampus.springsecuritypractice.service;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.coderscampus.springsecuritypractice.domain.User;
 import com.coderscampus.springsecuritypractice.repository.UserRepository;
 
 public class UserService implements UserDetailsService {
 
-    private PasswordEncoder passwordEncoder;
     private UserRepository userRepository;
     
-    public UserService(PasswordEncoder passwordEncoder, UserRepository userRepository) {
+    public UserService(UserRepository userRepository) {
         super();
-        this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
     }
 
